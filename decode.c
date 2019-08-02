c14eb5906f88b5cdf75358455a140dae13557caf
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-08-03 01:57:03 +0900

    Add opcodes

--- decode.c
+++ decode.c
@@ -28,10 +28,42 @@ void decode_get_r_type(uint32_t inst, struct r_type *r){
 }
 
 void decode_get_i_type(uint32_t inst, struct i_type *i){
+    i->rs1 = get_rs1(inst);
+    i->funct3 = get_funct3(inst);
+    i->rd = get_rd(inst);
+    i->opcode = get_opcode(inst);
 }
 
 void decode_get_s_type(uint32_t inst, struct s_type *s){
 }
 
 void decode_get_u_type(uint32_t inst, struct u_type *u){
+}
+
+void decode_get_b_type(uint32_t inst, struct b_type *i){
+}
+
+void decode_get_j_type(uint32_t inst, struct j_type *i){
+}
+
+uint32_t decode_get_i_imm(uint32_t inst){
+    uint32_t v = get_bits(inst, 20, 30);
+    uint8_t sign_bit = get_bits(inst, 31, 31);
+    for(int i=0;i<21;i++){
+        v += (sign_bit)<<(i+11);
+    }
+
+    return v;
+}
+
+uint32_t decode_get_s_imm(uint32_t inst){
+}
+
+uint32_t decode_get_b_imm(uint32_t inst){
+}
+
+uint32_t decode_get_u_imm(uint32_t inst){
+}
+
+uint32_t decode_get_j_imm(uint32_t inst){
 }
--- inst.h
+++ inst.h
@@ -11,7 +11,6 @@ struct r_type {
 };
 
 struct i_type {
-    uint16_t imm :12;
     uint8_t rs1 :5;
     uint8_t funct3 :3;
     uint8_t rd :5;
@@ -19,20 +18,28 @@ struct i_type {
 };
 
 struct s_type {
-    uint16_t imm_u :7;
     uint8_t rs1 :5;
     uint8_t rs2 :5;
     uint8_t funct3 :3;
-    uint8_t imm_l :5;
+    uint8_t opcode :7;
+};
+
+struct b_type {
+    uint8_t rs2 :5;
+    uint8_t rs1 :5;
+    uint8_t funct3 :3;
     uint8_t opcode :7;
 };
 
 struct u_type {
-    uint32_t imm :20;
     uint8_t rd :5;
     uint8_t opcode :7;
 };
 
+struct j_type {
+    uint8_t rd :5;
+    uint8_t opcode :7;
+};
 enum inst_opcode {
     OP_IMM = 0b0010011,
     OP_LUI = 0b0110111,
@@ -47,4 +54,60 @@ enum inst_opcode {
     OP_SYSTEM = 0b1110011,
 };
 
+enum funct3_opcode {
+    FUNCT3_JALR = 0b000,
+    FUNCT3_BEQ = 0b000,
+    FUNCT3_BNE = 0b001,
+    FUNCT3_BLT = 0b100,
+    FUNCT3_BGE = 0b101,
+    FUNCT3_BLTU = 0b110,
+    FUNCT3_BGEU = 0b111,
+    FUNCT3_LB = 0b000,
+    FUNCT3_LH = 0b001,
+    FUNCT3_LW = 0b010,
+    FUNCT3_LBU = 0b100,
+    FUNCT3_LHU = 0b101,
+    FUNCT3_SB = 0b000,
+    FUNCT3_SH = 0b001,
+    FUNCT3_SW = 0b010,
+    FUNCT3_ADDI = 0b000,
+    FNUCT3_SLTI = 0b010,
+    FUNCT3_SLTIU = 0b011,
+    FUNCT3_XORI = 0b100,
+    FUNCT3_ORI = 0b110,
+    FUNCT3_ANDI = 0b111,
+    FNUCT3_SLLI = 0b001,
+    FUNCT3_SRLI = 0b101,
+    FUNCT3_SRAI = 0b101,
+    FUNCT3_ADD = 0b000,
+    FUNCT3_SUB = 0b000,
+    FUNCT3_SLL = 0b001,
+    FUNCT3_SLT = 0b010,
+    FUNCT3_SLTU = 0b011,
+    FUNCT3_XOR = 0b100,
+    FUNCT3_SRL = 0b101,
+    FUNCT3_SRA = 0b101,
+    FUNCT3_OR = 0b110,
+    FUNCT3_AND = 0b111,
+    FUNCT3_FENCE = 0b000,
+    FUNCT3_ECALL = 0b000,
+    FUNCT3_EBREAK = 0b000
+};
+
+enum funct7_opcode {
+    FUNCT7_SLLI = 0b0000000,
+    FUNCT7_SRLI = 0b0000000,
+    FUNCT7_SRAI = 0b0100000,
+    FUNCT7_ADD = 0b0000000,
+    FUNCT7_SUB = 0b0100000,
+    FUNCT7_SLL = 0b0000000,
+    FUNCT7_SLT = 0b0000000,
+    FUNCT7_SLTU = 0b0000000,
+    FUNCT7_XOR = 0b0000000,
+    FUNCT7_SRL = 0b0000000,
+    FUNCT7_SRA = 0b0100000,
+    FUNCT7_OR = 0b0000000,
+    FUNCT7_AND = 0b0000000
+
+};
 #endif

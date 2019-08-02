9bc71e9719b99dd0685deecad6ec0534737bd13f
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-08-03 02:12:45 +0900

    able to decode ADDI

--- decode.h
+++ decode.h
@@ -1,4 +1,6 @@
 #ifndef DECODE_H
 #define DECODE_H
 uint8_t decode_get_opcode(uint32_t inst);
+void decode_get_i_type(uint32_t inst, struct i_type *i);
+uint32_t decode_get_i_imm(uint32_t inst);
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "inst.h"
+#include "decode.h"
 
 void print_bin(uint32_t v, int bit_len) {
     for(int i=bit_len;i>0;i--){
@@ -9,13 +10,23 @@ void print_bin(uint32_t v, int bit_len) {
 }
 
 int main(){
-    uint32_t test = 0b1111110000011111000111110000000; 
-    struct r_type *r = (struct r_type *)&test;
+    uint32_t inst = 0xfff08013;
 
-    print_bin(r->funct7, 6);
-    printf("\n");
-    print_bin(r->opcode, 6);
-    printf("\n");
-    printf("%X\n", r->funct7);
-    printf("%X\n", r->opcode);
+    switch(decode_get_opcode(inst)){
+        case OP_IMM:
+            printf("OP_IMM\n");
+            int32_t imm = (int32_t)decode_get_i_imm(inst);
+            struct i_type i_inst;
+            decode_get_i_type(inst, &i_inst);
+            switch(i_inst.funct3){
+                case FUNCT3_ADDI:
+                    printf("ADDI: imm:%d rs1:%d, rd:%d\n", imm, i_inst.rs1, i_inst.rd);
+                    break;
+                default:
+                    printf("Not Implemented!\n");
+            }
+            break;
+        default:
+            printf("Not Implemented!\n");
+    }
 }
